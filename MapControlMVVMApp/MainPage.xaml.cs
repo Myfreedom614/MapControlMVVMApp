3ee184e7aaf9d84830798859e7e03a0d25dd7c24
Franklin Chen <myfreedom614@gmail.com>
2015-08-25 01:22:58 +0800

    Add project

---
+++ MapControlMVVMApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MapControlMVVMApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Messenger.Default.Register<Geopoint>(this, Constants.SetMapViewToken, SetMapView);
+        }
+
+        private async void SetMapView(Geopoint point)
+        {
+            MyLocationPushpin.Visibility = Visibility.Visible;
+            await MainMap.TrySetViewAsync(point, 15, 0, 0, Windows.UI.Xaml.Controls.Maps.MapAnimationKind.Bow);
+        }
+    }
+}
---
+++ MapControlMVVMApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,110 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using GalaSoft.MvvmLight.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI.Popups;
+
+namespace MapControlMVVMApp.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public const string MyLocationPropertyName = "MyLocation";
+
+        private Geoposition _myLocation = null;
+
+        public Geoposition MyLocation
+        {
+            get
+            {
+                return _myLocation;
+            }
+
+            set
+            {
+                if (_myLocation == value)
+                {
+                    return;
+                }
+
+
+                _myLocation = value;
+                RaisePropertyChanged(MyLocationPropertyName);
+            }
+        }
+
+        public const string GetMyLocationCommandPropertyName = "GetMyLocationCommand";
+
+        private RelayCommand _getMyLocationCommand = null;
+
+        public RelayCommand GetMyLocationCommand
+        {
+            get
+            {
+                return _getMyLocationCommand;
+            }
+
+            set
+            {
+                if (_getMyLocationCommand == value)
+                {
+                    return;
+                }
+
+
+                _getMyLocationCommand = value;
+                RaisePropertyChanged(GetMyLocationCommandPropertyName);
+            }
+        }
+
+        public MainViewModel()
+        {
+            GetMyLocationCommand = new RelayCommand(GetMyLocation);
+        }
+
+        private async void ToggleProgressBar(bool toggle, string message = "")
+        {
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.StatusBar"))
+            {
+                var statusBar = Windows.UI.ViewManagement.StatusBar.GetForCurrentView();
+                
+                if (toggle)
+                {
+                    statusBar.ProgressIndicator.Text = message;
+                    await statusBar.ProgressIndicator.ShowAsync();
+                }
+                else
+                {
+                    await statusBar.ProgressIndicator.HideAsync();
+                }
+            }
+            
+        }
+
+        private async void GetMyLocation()
+        {
+            try
+            {
+                ToggleProgressBar(true, "Getting your location");
+
+                Geolocator locator = new Geolocator();
+
+                var location = await locator.GetGeopositionAsync(TimeSpan.FromMilliseconds(1), TimeSpan.FromSeconds(60));
+                MyLocation = location;
+
+                Messenger.Default.Send<Geopoint>(MyLocation.Coordinate.Point, Constants.SetMapViewToken);
+
+                ToggleProgressBar(false);
+            }
+            catch (Exception ex)
+            {
+                MessageDialog Dialog = new MessageDialog("Location detection failed");
+                Dialog.ShowAsync();
+            }
+        }
+    }
+}
